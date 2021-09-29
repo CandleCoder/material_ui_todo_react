d3dd7d14929135b2f9fceffdaa2805e75017b8f8
CandleCoder <manikantupadhyay@gmail.com>
2021-09-29 17:58:41 +0530

    Added changes

--- src/Components/ContentContainer.js
+++ src/Components/ContentContainer.js
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
-import data from '../Data/TodoData.json';
 import TakeANoteContainer from './TakeANoteComponent';
 
 export default function ContentContainer() {
-	console.log(data);
 	return (
 		<React.Fragment>
 			<CssBaseline />
--- src/Components/NotePaperComponent.js
+++ src/Components/NotePaperComponent.js
@@ -1,17 +1,12 @@
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
-const bull = (
-	<Box component="span" sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}>
-		•
-	</Box>
-);
+import data from '../Data/TodoData.json';
 
 export default function NotePaperComponent() {
+	console.log(data);
 	return (
 		<Card
 			variant="outlined"
@@ -22,14 +17,8 @@ export default function NotePaperComponent() {
 			}}
 		>
 			<CardContent>
-				<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-					Word of the Day
-				</Typography>
 				<Typography variant="h5" component="div">
-					be{bull}nev{bull}o{bull}lent
-				</Typography>
-				<Typography sx={{ mb: 1.5 }} color="text.secondary">
-					adjective
+					benevolent
 				</Typography>
 				<Typography variant="body2">
 					well meaning and kindly.
