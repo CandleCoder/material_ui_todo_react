13e83e250ea2330de39bb34e82bf58ef85bf90ab
CandleCoder <manikantupadhyay@gmail.com>
2021-09-30 18:11:03 +0530

    Adding changes for Note pallete

--- src/Components/NotePaperComponent.js
+++ src/Components/NotePaperComponent.js
@@ -1,9 +1,10 @@
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import data from '../Data/TodoData.json';
+import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
 
 export default function NotePaperComponent() {
 	return data.map((v) => {
@@ -11,6 +12,7 @@ export default function NotePaperComponent() {
 			<span>
 				{
 					<Card
+						key={v.id}
 						variant="outlined"
 						sx={{
 							maxWidth: 200,
@@ -18,18 +20,18 @@ export default function NotePaperComponent() {
 							margin: '12px'
 						}}
 					>
+						<IconButton variant="outlined" aria-label="Select" component="span">
+							<CheckCircleRoundedIcon />
+						</IconButton>
 						<CardContent>
-							<Typography variant="h6" component="div">
+							<Typography variant="h6" component="div" key={v.id + 'Typography1'}>
 								{v.title}
 							</Typography>
-							<Typography variant="body2">
+							<Typography variant="body2" key={v.id + 'Typography2'}>
 								{v.Content}
 								<br />
 							</Typography>
 						</CardContent>
-						<CardActions>
-							<Button size="small">Learn More</Button>
-						</CardActions>
 					</Card>
 				}
 			</span>
